8c0c515da206ff69fced03b20d7d3df86a298592
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-19 15:13:12 -0400

    Initial Xcode project setup

---
+++ BackTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackTrack
+//
+//  Created by Ben DiFrancesco on 5/19/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
