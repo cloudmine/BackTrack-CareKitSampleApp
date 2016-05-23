27baa0ddc164245006d0342ce2ec87afb87259fe
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-23 16:43:25 -0400

    Add a brisk walk intervention activity

--- BackTrack/Models/BCMActivities.m
+++ BackTrack/Models/BCMActivities.m
@@ -1,23 +1,24 @@
 #import "BCMActivities.h"
 
+static NSString *const ExerciseInterventionsGroupIdentifier = @"BCMExerciseInterventions";
+
 @implementation BCMActivities
 
 #pragma mark Public
 
 + (NSArray<OCKCarePlanActivity *> *)activities
 {
-    return @[self.hamstringStretchIntervention];
+    return @[self.hamstringStretchIntervention, self.briskWalkIntervention];
 }
 
 #pragma mark Generators
 
 + (OCKCarePlanActivity *_Nonnull)hamstringStretchIntervention
 {
-    NSDateComponents *components = [[NSCalendar currentCalendar] componentsInTimeZone:[NSTimeZone defaultTimeZone] fromDate:[NSDate new]];
-    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:components occurrencesPerDay:3];
+    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:self.todaysDateComponents occurrencesPerDay:3];
 
     return [[OCKCarePlanActivity alloc] initWithIdentifier:@"BCMHamstringStretch"
-                                           groupIdentifier:@"BCMExerciseInterventions"
+                                           groupIdentifier:ExerciseInterventionsGroupIdentifier
                                                       type:OCKCarePlanActivityTypeIntervention
                                                      title:NSLocalizedString(@"Hamstring Stretch", nil)
                                                       text:nil
@@ -27,6 +28,30 @@
                                                   schedule:schedule
                                           resultResettable:YES
                                                   userInfo:nil];
-        }
+}
+
++ (OCKCarePlanActivity *_Nonnull)briskWalkIntervention
+{
+    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:self.todaysDateComponents occurrencesPerDay:1];
+
+    return [[OCKCarePlanActivity alloc] initWithIdentifier:@"BCMBriskWalk"
+                                           groupIdentifier:ExerciseInterventionsGroupIdentifier
+                                                      type:OCKCarePlanActivityTypeIntervention
+                                                     title:NSLocalizedString(@"Brisk Walk", nil)
+                                                      text:NSLocalizedString(@"15 minutes", nil)
+                                                 tintColor:nil
+                                              instructions:nil
+                                                  imageURL:nil
+                                                  schedule:schedule
+                                          resultResettable:YES
+                                                  userInfo:nil];
+}
+
+#pragma mark Helpers
+
++ (NSDateComponents *_Nonnull)todaysDateComponents
+{
+    return [[NSCalendar currentCalendar] componentsInTimeZone:[NSTimeZone defaultTimeZone] fromDate:[NSDate new]];
+}
 
 @end
