bd77e450e6e2892040bbe037d4e3a99c6f0980ed
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-23 15:42:47 -0400

    Load the symptom tracker view controller on the symptom tab

---
+++ BackTrack/ViewControllers/BMCSymptomNavController.h
@@ -0,0 +1,13 @@
+//
+//  BMCSymptomNavController.h
+//  BackTrack
+//
+//  Created by Ben DiFrancesco on 5/23/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMCSymptomNavController : UINavigationController
+
+@end
---
+++ BackTrack/ViewControllers/BMCSymptomNavController.m
@@ -0,0 +1,20 @@
+#import "BMCSymptomNavController.h"
+#import <CareKit/CareKit.h>
+#import "BCMMainTabController.h"
+#import "UIViewController+BCM.h"
+
+@interface BMCSymptomNavController ()
+
+@end
+
+@implementation BMCSymptomNavController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    OCKSymptomTrackerViewController *symptomVC = [[OCKSymptomTrackerViewController alloc] initWithCarePlanStore:self.bcmTabBarController.carePlanStore];
+    self.viewControllers = @[symptomVC];
+}
+
+@end
