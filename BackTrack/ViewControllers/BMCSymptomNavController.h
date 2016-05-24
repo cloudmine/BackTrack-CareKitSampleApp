770f627ebf986f71bc0e28ff679439bfb11e8624
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-24 17:24:10 -0400

    Implement the symptom tracker delegate to receive notifications when the user taps an assessment activity

--- BackTrack/ViewControllers/BMCSymptomNavController.h
+++ BackTrack/ViewControllers/BMCSymptomNavController.h
@@ -1,11 +1,3 @@
-//
-//  BMCSymptomNavController.h
-//  BackTrack
-//
-//  Created by Ben DiFrancesco on 5/23/16.
-//  Copyright © 2016 CloudMine. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface BMCSymptomNavController : UINavigationController
--- BackTrack/ViewControllers/BMCSymptomNavController.m
+++ BackTrack/ViewControllers/BMCSymptomNavController.m
@@ -2,20 +2,30 @@
 #import <CareKit/CareKit.h>
 #import "BCMMainTabController.h"
 #import "UIViewController+BCM.h"
+#import <CareKit/CareKit.h>
 
-@interface BMCSymptomNavController ()
+@interface BMCSymptomNavController ()<OCKSymptomTrackerViewControllerDelegate>
 
 @end
 
 @implementation BMCSymptomNavController
+
+#pragma mark Lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
     OCKSymptomTrackerViewController *symptomVC = [[OCKSymptomTrackerViewController alloc] initWithCarePlanStore:self.bcmTabBarController.carePlanStore];
-
+    symptomVC.delegate = self;
     [self showViewController:symptomVC sender:nil];
+}
+
+#pragma mark OCKSymptomTrackerViewControllerDelegate
+
+- (void)symptomTrackerViewController:(OCKSymptomTrackerViewController *)viewController didSelectRowWithAssessmentEvent:(OCKCarePlanEvent *)assessmentEvent
+{
+    NSLog(@"Selected Assessment: %@", assessmentEvent.activity.identifier);
 }
 
 @end
