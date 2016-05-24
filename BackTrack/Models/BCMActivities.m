89271838c7d5f8bbf582884278b3a3ea3d5f2756
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-24 15:33:26 -0400

    Add assessment activities to care plan

--- BackTrack/Models/BCMActivities.m
+++ BackTrack/Models/BCMActivities.m
@@ -1,7 +1,9 @@
 #import "BCMActivities.h"
 
 static NSString *const ExerciseInterventionsGroupIdentifier   = @"BCMExerciseInterventions";
-static NSString *const MedicationInterventionsGroupIdentifier = @"BCMExerciseInterventions";
+static NSString *const MedicationInterventionsGroupIdentifier = @"BCMMedicationInterventions";
+static NSString *const SubjectiveAssessmentsGroupIdentifier   = @"BCMSubjectiveAssessments";
+static NSString *const ObjectiveAssessmentsGroupIdentifier    = @"BCMObjectiveAssessments";
 
 @implementation BCMActivities
 
@@ -10,7 +12,9 @@ static NSString *const MedicationInterventionsGroupIdentifier = @"BCMExerciseInt
 + (NSArray<OCKCarePlanActivity *> *)activities
 {
     return @[self.hamstringStretchIntervention, self.briskWalkIntervention,
-             self.warmCompressIntervention, self.painKillerIntervention];
+             self.warmCompressIntervention, self.painKillerIntervention,
+             self.painTrackAssessment, self.moodTrackAssessment,
+             self.weightTrackAssessment];
 }
 
 #pragma mark Generators
@@ -23,7 +27,7 @@ static NSString *const MedicationInterventionsGroupIdentifier = @"BCMExerciseInt
                                            groupIdentifier:ExerciseInterventionsGroupIdentifier
                                                       type:OCKCarePlanActivityTypeIntervention
                                                      title:NSLocalizedString(@"Hamstring Stretch", nil)
-                                                      text:nil
+                                                      text:NSLocalizedString(@"5 minutes", nil)
                                                  tintColor:nil
                                               instructions:nil
                                                   imageURL:nil
@@ -58,7 +62,7 @@ static NSString *const MedicationInterventionsGroupIdentifier = @"BCMExerciseInt
                                            groupIdentifier:MedicationInterventionsGroupIdentifier
                                                       type:OCKCarePlanActivityTypeIntervention
                                                      title:NSLocalizedString(@"Warm Compress", nil)
-                                                      text:nil
+                                                      text:NSLocalizedString(@"Morning", nil)
                                                  tintColor:nil
                                               instructions:nil
                                                   imageURL:nil
@@ -79,6 +83,57 @@ static NSString *const MedicationInterventionsGroupIdentifier = @"BCMExerciseInt
                                                       type:OCKCarePlanActivityTypeIntervention
                                                      title:NSLocalizedString(@"Ibuprofen", nil)
                                                       text:NSLocalizedString(@"200 mg, Morning/Evening", nil)
+                                                 tintColor:nil
+                                              instructions:nil
+                                                  imageURL:nil
+                                                  schedule:schedule
+                                          resultResettable:YES
+                                                  userInfo:nil];
+}
+
++ (OCKCarePlanActivity *_Nonnull)painTrackAssessment
+{
+    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:self.todaysDateComponents occurrencesPerDay:1];
+
+    return [[OCKCarePlanActivity alloc] initWithIdentifier:@"BCMPainTrack"
+                                           groupIdentifier:SubjectiveAssessmentsGroupIdentifier
+                                                      type:OCKCarePlanActivityTypeAssessment
+                                                     title:NSLocalizedString(@"Pain", nil)
+                                                      text:NSLocalizedString(@"Lower Back", nil)
+                                                 tintColor:nil
+                                              instructions:nil
+                                                  imageURL:nil
+                                                  schedule:schedule
+                                          resultResettable:YES
+                                                  userInfo:nil];
+}
+
++ (OCKCarePlanActivity *_Nonnull)moodTrackAssessment
+{
+    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:self.todaysDateComponents occurrencesPerDay:1];
+
+    return [[OCKCarePlanActivity alloc] initWithIdentifier:@"BCMMoodTrack"
+                                           groupIdentifier:SubjectiveAssessmentsGroupIdentifier
+                                                      type:OCKCarePlanActivityTypeAssessment
+                                                     title:NSLocalizedString(@"Mood", nil)
+                                                      text:nil
+                                                 tintColor:nil
+                                              instructions:nil
+                                                  imageURL:nil
+                                                  schedule:schedule
+                                          resultResettable:YES
+                                                  userInfo:nil];
+}
+
++ (OCKCarePlanActivity *_Nonnull)weightTrackAssessment
+{
+    OCKCareSchedule *schedule = [OCKCareSchedule dailyScheduleWithStartDate:self.todaysDateComponents occurrencesPerDay:1];
+
+    return [[OCKCarePlanActivity alloc] initWithIdentifier:@"BCMWeightTrack"
+                                           groupIdentifier:ObjectiveAssessmentsGroupIdentifier
+                                                      type:OCKCarePlanActivityTypeAssessment
+                                                     title:NSLocalizedString(@"Weight", nil)
+                                                      text:NSLocalizedString(@"Early Morning", nil)
                                                  tintColor:nil
                                               instructions:nil
                                                   imageURL:nil
