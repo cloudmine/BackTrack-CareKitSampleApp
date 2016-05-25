59e026904db89c04fb2ac00523362d805336bc06
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-25 16:32:13 -0400

    Add a test insight message to the insights screen

--- BackTrack/ViewControllers/BCMInsightsNavController.m
+++ BackTrack/ViewControllers/BCMInsightsNavController.m
@@ -11,7 +11,12 @@
 {
     [super viewDidLoad];
 
-    OCKInsightsViewController *insightsVC = [[OCKInsightsViewController alloc] initWithInsightItems:@[]
+    OCKMessageItem *testMessage = [[OCKMessageItem alloc] initWithTitle:NSLocalizedString(@"Perform your exercises!", nil)
+                                                                   text:NSLocalizedString(@"Staying active is the number one way to mitigate your pain", nil)
+                                                              tintColor:nil
+                                                            messageType:OCKMessageItemTypeTip];
+
+    OCKInsightsViewController *insightsVC = [[OCKInsightsViewController alloc] initWithInsightItems:@[testMessage]
                                                                                         headerTitle:NSLocalizedString(@"Treatment Insights", nil)
                                                                                      headerSubtitle:NSLocalizedString(@"Data from your treatment over the last week", nil)];
     [self showViewController:insightsVC sender:nil];
